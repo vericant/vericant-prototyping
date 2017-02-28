137f6788a87249ebcc0f34dd37a682f88c241dce
Aaron Baideme <a@baide.me>
2017-02-28 14:20:09 +0800

    Changes to Taiga 515 prototype + Scheduler++ framework

---
+++ src/helpers/partial.js
@@ -0,0 +1,20 @@
+var Handlebars = require('handlebars');
+
+/**
+ * Loads the corresponding partial
+ * @param {String} partial - The partial name.
+ * @param {Object} options - Handlebars object.
+ * @example
+ * {{partial 'test1'}}
+ * @returns {String} compiled partial
+ */
+
+module.exports = function(partial, options) {
+  var html = Handlebars.partials[partial];
+
+  if (html === undefined) {
+    return 'blah';
+  }
+
+  return html;
+}
